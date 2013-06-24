83b2fcc407bb7e6530dae3bfabf559e3d26e605a
Shivam Shah <mathwizard98@gmail.com>
2013-06-23 17:44:19 -0700

    digiflower-Shivam shah

--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/PentagonCrazy.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/PentagonCrazy.java
@@ -1,10 +1,13 @@
 package org.teachingkidsprogramming.recipes;
 
+import org.teachingextensions.logo.Tortoise;
+
 public class PentagonCrazy
 {
   public static void main(String[] args)
   {
     //    Make the tortoise move as fast as possible --#3
+    Tortoise.setSpeed(10);
     //    createColorPalette (recipe below) --#8
     //    ------------- Recipe for createColorPalette --#8
     //    Add steel blue to the color wheel --#7
@@ -16,6 +19,9 @@ public class PentagonCrazy
     //    DrawPentagon (recipe below) --#10
     //    ------------- Recipe for drawPentagon --#10
     //    Do the following 200 times --#2
+    for (int i = 1; i <= 200; i++)
+    {
+    }
     //        AdjustPen (recipe below) --#9
     //        ------------- Recipe for adjustPen --#9
     //        Change the color of the line the tortoise draws to the next color on the color wheel --#6
@@ -25,6 +31,7 @@ public class PentagonCrazy
     //        ------------- End of adjustPen recipe 
     //        Move the tortoise the length of a side --#4
     //        Turn the tortoise 1/5th of 360 degrees --#1
+    Tortoise.turn(360 / 5);
     //        Turn the tortoise 1 more degree --#5
     //    Repeat
     //    ------------- End of drawPentagon recipe
--- TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/recipes/SimpleSquare.java
@@ -2,12 +2,24 @@ package org.teachingkidsprogramming.recipes;
 
 import org.teachingextensions.logo.Colors;
 import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.windows.MessageBox;
 
 public class SimpleSquare
 {
   public static void main(String[] args) throws Exception
   {
     Tortoise.show();
+<<<<<<< HEAD
+    Tortoise.setSpeed(10);
+    int side = MessageBox.askForNumericalInput("how many sides would you like?");
+    for (int i = 1; i <= side; i++)
+    {
+      Tortoise.setPenColor(Colors.getRandomColor());
+      Tortoise.setPenWidth(i * 3.5);
+      Tortoise.move(i * 5);
+      Tortoise.turn(360.0 * -3 / side);
+    }
+=======
     //  Make the tortoise move as fast as possible --#6
     //  Do the following 4 times --#5.1
     Tortoise.setPenColor(Colors.Blues.Blue);
@@ -23,5 +35,6 @@ public class SimpleSquare
     Tortoise.move(50);
     Tortoise.turn(90);
     //  Repeat --#5.2
+>>>>>>> parent of 22df09a... refactor square-Shivam/Mihir
   }
 }
